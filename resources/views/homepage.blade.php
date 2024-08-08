7c0a229b4c01dbc28a53ae9fc45fe8f5cfb81e5d
DunJik005 <aca.dunjic111@gmail.com>
2024-08-08 15:53:56 +0200

    treca verzija bloga

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -6,6 +6,7 @@
     <h1>RAF IT Days Laravel Blog</h1>
     <p>Dobrodošli na zvanični blog RAF IT Days-a! Ovaj blog je posvećen najnovijim trendovima u IT industriji, vestima sa našeg fakulteta, i savetima za studente i profesionalce. Pročitajte naše najnovije članke i budite u toku sa svim što se dešava u svetu tehnologije i obrazovanja.</p>
     <p>Ovde prikazujemo sve zapise iz bloga:</p>
+<p> blablablalba </p>
 
     @foreach($blogovi as $blog)
         <div class="mb-4">
